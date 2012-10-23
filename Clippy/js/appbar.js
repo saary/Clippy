f0e7904d7700f04e37b03089e882377b120cf6bd
Kashtan <kashtang@gmail.com>
2012-10-23 15:40:09 +0200

    appbar.js

---
+++ Clippy/js/appbar.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+    "use strict";
+    var page = WinJS.UI.Pages.define("default.html", {
+        ready: function (element, options) {
+            document.getElementById("cmdFilePicker").addEventListener("click", musicPlayer.pickMusicFilesToPlay, false);
+          
+            WinJS.log && WinJS.log("To show the bar, swipe up from " +
+                "the bottom of the screen, right-click, or " +
+                "press Windows Logo + z. To dismiss the bar, " +
+                "tap in the application, swipe, right-click, " +
+                "or press Windows Logo + z again.", "sample", "status");
+        },
+    });
+}).call(this, jQuery);
